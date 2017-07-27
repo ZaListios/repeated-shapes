54fcebc1f62dcf0c61e80396a8a8a7479593e22e
ZaListios <14lroberts@conyers.org.uk>
2017-07-27 19:07:03 +0000

    Update and rename .gitignore to repeated_shape.py

---
+++ .gitignore/repeated_shape.py
@@ -0,0 +1,25 @@
+from turtle import *
+import math, random
+speed(0)
+hideturtle()
+'''
+change the values to whatever
+
+here is an example of one that you could use (most combinations won't look
+like this).
+l = 100
+a = 90
+r = 4
+'''
+l = 
+a = 
+r = 
+aII = 
+threesixtyofaii = math.floor(360 / aII)
+def shape():
+    for i in range(r):
+        fd(l)
+        rt(a)
+for i in range(threesixtyofaii):
+    shape()
+    rt(aII)
